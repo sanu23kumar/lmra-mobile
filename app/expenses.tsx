d249bb90ecf1b0f50919e772fa8d15090f121ea1
Sanu Kumar <sanu23kumar@gmail.com>
2024-11-05 02:52:02 +0530

    add expenses route

---
+++ app/expenses.tsx
@@ -0,0 +1,28 @@
+import AddExpense from "@/components/AddExpense";
+import { ThemedView } from "@/components/ThemedView";
+import UserExpense from "@/components/UserExpense";
+import { Expense } from "@/database/tables/expenses/expenses.types";
+import { useExpenses } from "@/database/tables/expenses/useExpenses";
+import { FlatList } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Expenses() {
+  const { bottom } = useSafeAreaInsets();
+  const { expenses } = useExpenses();
+
+  const renderItem = ({ item }: { item: Expense }) => (
+    <UserExpense item={item} />
+  );
+
+  return (
+    <ThemedView style={{ flex: 1, paddingBottom: bottom }}>
+      <FlatList
+        data={expenses}
+        keyExtractor={(expense) => expense.id + ""}
+        renderItem={renderItem}
+        contentContainerStyle={{ gap: 16 }}
+      />
+      <AddExpense />
+    </ThemedView>
+  );
+}
