2c6d7ca167a934722390226aa43f450291cff4d8
Sanu Kumar <sanu23kumar@gmail.com>
2024-11-05 02:51:40 +0530

    add database provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
+import Header from "@/components/Header";
+import { DatabaseProvider } from "@/database";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <DatabaseProvider>
+      <Stack
+        screenOptions={{
+          header: Header,
+        }}
+      >
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+        <Stack.Screen
+          name="expenses"
+          options={{
+            title: "Expenses",
+          }}
+        />
+      </Stack>
+    </DatabaseProvider>
   );
 }
