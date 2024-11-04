af3867ffbffc32fdb4cc26c19f1eb38f28dc92a8
Sanu Kumar <sanu23kumar@gmail.com>
2024-11-05 02:53:57 +0530

    add useExpenses

--- app/(tabs)/finances.tsx
+++ app/(tabs)/finances.tsx
@@ -2,9 +2,12 @@ import FinanceCategories from "@/components/FinanceCategories";
 import FinanceSection from "@/components/FinanceSection";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { StyleSheet, ScrollView } from "react-native";
+import { useExpenses } from "@/database/tables/expenses/useExpenses";
+import { router } from "expo-router";
+import { StyleSheet, ScrollView, Pressable, View } from "react-native";
 
 export default function FinancesScreen() {
+  const { totalExpense } = useExpenses();
   const expenseCategories = [
     "Eating Out 30,000",
     "Groceries 10,000",
@@ -28,40 +31,54 @@ export default function FinancesScreen() {
   ];
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="default" style={styles.text}>
-        October
-      </ThemedText>
       <ScrollView
         style={styles.scrollView}
         contentContainerStyle={styles.scrollViewContainer}
+        showsVerticalScrollIndicator={false}
       >
         {/* Expenses section */}
         <FinanceSection
           title={"Expenses"}
-          total="1,39,000"
+          total={totalExpense.toString()}
           categories={expenseCategories}
+          onPress={() => router.navigate("/expenses")}
         />
         <FinanceSection
           title={"Savings"}
           total="80,000"
           categories={savingsCategories}
+          onPress={() => router.navigate("/add_expense")}
         />
         <FinanceSection
           title={"EMIs"}
           total="32,000"
           categories={emisCategories}
+          onPress={() => router.navigate("/add_expense")}
         />
         <FinanceSection
           title={"Income"}
           total="2,00,000"
           categories={incomeCategories}
+          onPress={() => router.navigate("/add_expense")}
         />
         <FinanceSection
           title={"Credit Card Payments"}
           total="1,00,000"
           categories={creditCardPaymentsCategories}
+          onPress={() => router.navigate("/add_expense")}
         />
       </ScrollView>
+      <View style={styles.monthContainer}>
+        <Pressable style={styles.monthButton}>
+          <ThemedText style={styles.text}>September</ThemedText>
+        </Pressable>
+        <ThemedText type="subtitle" style={styles.text}>
+          October
+        </ThemedText>
+        <Pressable style={styles.monthButton}>
+          <ThemedText style={styles.text}>November</ThemedText>
+        </Pressable>
+      </View>
     </ThemedView>
   );
 }
@@ -69,6 +86,13 @@ export default function FinancesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  monthButton: {
+    opacity: 0.6,
+  },
+  monthContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   section: {
     flexDirection: "column",
@@ -80,8 +104,9 @@ const styles = StyleSheet.create({
     gap: 16,
   },
   text: {
-    paddingHorizontal: 16,
-    paddingBottom: 16,
+    padding: 16,
     color: "#CD853F",
+    flex: 1,
+    textAlign: "center",
   },
 });
