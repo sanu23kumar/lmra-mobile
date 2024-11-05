539e78d74b39e3db6a3e49ae57e321fc3110012e
Sanu Kumar <sanu23kumar@gmail.com>
2024-11-05 13:29:20 +0530

    add themmed view

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,25 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { Colors } from "@/constants/Colors";
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  colorScheme?: keyof typeof Colors.light;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+export function ThemedView({
+  style,
+  lightColor,
+  darkColor,
+  colorScheme = "background",
+  ...otherProps
+}: ThemedViewProps) {
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    colorScheme
+  );
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
