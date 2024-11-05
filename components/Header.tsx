f78487e502671bb49259ce0d49e68d75c2321b26
Sanu Kumar <sanu23kumar@gmail.com>
2024-11-05 13:30:28 +0530

    refactor Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,21 @@
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
-import { Ionicons } from "@expo/vector-icons";
 import { Pressable } from "react-native";
+import { ThemedIcon } from "./ThemedIcon";
 
 export default function Header(props: any) {
   const { top } = useSafeAreaInsets();
   return (
-    <ThemedView style={{ padding: 16, paddingTop: top, flexDirection: "row" }}>
+    <ThemedView
+      style={{ padding: 16, paddingTop: top, flexDirection: "row", gap: 12 }}
+    >
       {props.navigation.canGoBack() ? (
         <Pressable onPress={props.navigation.goBack}>
-          <Ionicons name="arrow-back" size={24} color={"#CD853F"} />
+          <ThemedIcon name="arrow-back" colorScheme="primary" />
         </Pressable>
       ) : null}
-      <ThemedText
-        type="title"
-        style={{
-          fontFamily: "Poppins",
-          color: "#CD853F",
-          paddingLeft: 12,
-        }}
-      >
+      <ThemedText type="title" colorScheme="primary">
         {props.options.title}
       </ThemedText>
     </ThemedView>
